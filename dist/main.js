12c1786b7e2163755e147bbb5ce107b4f7379757
NamaWorks <pedronayamartinez@hotmail.com>
2025-07-14 23:39:57 +0200

    create first version with ssr and client functionalities

--- dist/main.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("test"),document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("h1");console.log(e)})})();
-//# sourceMappingURL=main.js.map
